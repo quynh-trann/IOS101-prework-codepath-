71b9efaa1d74ed715b7625648d7f1c7780c4ac94
Quynh Tran <callmequynh.04@gmail.com>
2025-04-28 19:39:39 -0400

    added prework to repository

---
+++ IOS101-preworkTests/IOS101_preworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOS101_preworkTests.swift
+//  IOS101-preworkTests
+//
+//  Created by Quynh Tran on 4/27/25.
+//
+
+import Testing
+@testable import IOS101_prework
+
+struct IOS101_preworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ IOS101-preworkUITests/IOS101_preworkUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  IOS101_preworkUITestsLaunchTests.swift
+//  IOS101-preworkUITests
+//
+//  Created by Quynh Tran on 4/27/25.
+//
+
+import XCTest
+
+final class IOS101_preworkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
